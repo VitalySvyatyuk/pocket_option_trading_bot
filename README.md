0a31ee51fcc5e6334381e52cacc10fe8967251f7
Vitaly Svyatyuk <vitalysvyatyuk@gmail.com>
2023-07-23 11:14:38 +0300

    Remove indicators, cleanup a code
    Fix README, remove stock_indicators from the project, .NET6 is not needed anymore.

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# pocket_option_trading_bot
+# Pocket Option Trading Bot with Machine Learning
 Bot for autotrade in [Pocket Option](https://pocketoption.com/). Only for Demo mode!
 
 ### Installation
-Install packages from requirements.txt file. For using stock_indicators package,
-.NET6 must be in the system: https://dotnet.microsoft.com/en-us/download/dotnet/6.0
+`pip3 install -r requirements.txt`
 
 ### Run
 `python3 po_bot.py`
@@ -15,29 +14,30 @@ Install packages from requirements.txt file. For using stock_indicators package,
 
 ### Information
 Bot is for learning purposes only! And only for Demo mode!
-The current version uses machine learning for price prediction. Each row in dataframe 
-includes price from the previous 20 minutes. Plus current price. 21 params in total.
-From the beginning we have 3000 seconds with prices from the history. This is pretty
-enough for teaching model and testing it. As you may see, I'm not using any indicators
-now, only price movement is enough for the model. Also, I'm not using normalization/scale
-for the params, as all of them have similar values.
+The current version uses machine learning for price prediction.
+Each row in dataframe includes the price from the previous 20 minutes.
+Plus current price. 21 params in total. From the beginning, we have 3000 seconds
+with prices from the history. This is pretty enough for teaching the model and
+testing it. As you may see, I'm not using any indicators now, only price movement
+is enough for the model. Also, I'm not using normalization/scale for the params,
+as they all have similar values
 
 What happens each second? 
 - Each second bot teaches a model with the last data from the stack.
 - Each second bot makes a prediction for the last price.
-- Each second you may see a record 'Accuracy' 0.4-0.8 in console.
+- Each second you may see a record 'Accuracy' 0.4-0.8 in the console.
 - Each second bot calls or puts based on prediction. This activity is regulated by `MAX_ACTIONS` and  `ACTIONS_SECONDS` params, see below.
 
-Too many things happen each second, that's why your processor will be loaded on 100%. I'll
-fix that in one of the next versions.
-I suggest choosing a currency with the highest accuracy value. In fact, the currency with 
-the accuracy 0.7 will give you 55% of profitable results. If you see an accuracy around 0.5-0.6,
-feel free to switch to another currency.
+Too many things happen each second, that's why your processor will be loaded at 100%.
+I'll fix that in one of the next versions.
+I suggest choosing a currency with the highest accuracy value. In fact, the currency
+with an accuracy of 0.7 will give you 55% of profitable results. If you see an accuracy
+around 0.5-0.6, feel free to switch to another currency.
 
 Bot connects to websocket and receives signals every half a second from PO.
-To make it more strict, I simplify data to 1 second so that to use seconds 
-everywhere. After each changing of currency the screen reloads. It is to
-cut unwanted signals from previous currencies.
+To make it more convenient, I simplify data to 1 second so that to use seconds
+everywhere. After each change of currency, the screen reloads. It is to cut
+unwanted signals from previous currencies.
 
 By default, Bot makes no more than 1 order in 15 seconds if conditions are met.
 `MAX_ACTIONS` - amount of orders in `ACTIONS_SECONDS`. For example, if 
--- po_bot.py
+++ po_bot.py
@@ -1,10 +1,6 @@
 import base64
-import csv
 import json
-import os
 import random
-import time
-# import winsound
 from datetime import datetime, timedelta
 from decimal import Decimal
 
@@ -13,14 +9,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from stock_indicators import indicators
-from stock_indicators.indicators.common.quote import Quote
+
 
 LENGTH_STACK_MIN = 1300
-LENGTH_STACK_MAX = 4500  # 1 hour
+LENGTH_STACK_MAX = 4000  # 1 hour
 PERIOD = 15  # PERIOD on the graph
 STACK = {}  # {1687021970: 0.87, 1687021971: 0.88}
 ACTIONS = []  # list of {datetime: value} when an action has been made
@@ -31,11 +25,6 @@ CURRENCY = None
 CURRENCY_CHANGE = False
 CURRENCY_CHANGE_DATE = datetime.now()
 HISTORY_TAKEN = False  # becomes True when history is taken. History length is 900-1800
-SMA_LONG = 60
-SMA_SHORT = 4
-SMMA_SHORT = 10
-CCI_PERIOD = 20
-CCI = 100
 CLOSED_TRADES_LENGTH = 3
 HEADER = [
     'val1200', 'val1140', 'val1080', 'val1020', 'val960', 'val900', 'val840', 'val780', 'val720', 'val660',
@@ -73,7 +62,7 @@ companies = {
 }
 
 
-def LoadWebDriver():
+def load_web_driver():
     # TODO: make currency changing smoother
     base_url = "https://pocketoption.com/en/cabinet/demo-quick-high-low/"  # Change the site
     driver.get(base_url)
@@ -110,70 +99,11 @@ def do_action(signal):
 
     if action:
         try:
-            # winsound.Beep(440, 300)
             print(f"date: {datetime.now().strftime('%H:%M:%S')} do {signal.upper()}, currency: {CURRENCY} last_price: {list(STACK.values())[-1]}")
             driver.find_element(by=By.CLASS_NAME, value=f'btn-{signal}').click()
             ACTIONS.append(datetime.now())
         except Exception as e:
             print(e)
-
-
-def gather_statistics(value, action, adx, cci, rsi, macd, psar, mass):
-    with open(f'stats/{CURRENCY.replace("/", "")}.csv', 'a+') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',')
-        csv_writer.writerow(
-            [
-                CURRENCY,
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                value,
-                action,
-                adx.adx,
-                adx.adxr,
-                adx.pdi,
-                adx.mdi,
-                cci.cci,
-                rsi.rsi,
-                macd.macd,
-                macd.signal,
-                psar.sar,
-                psar.is_reversal,
-                mass,
-                '',
-            ]
-        )
-
-
-def get_quotes(stack, step=1):
-    if len(stack) // 100 == len(stack) / 100 and len(stack) < LENGTH_STACK_MAX:
-        print(len(stack))
-    quotes = []
-    quotes_length = (len(stack) // step) * step  # 507 to 505
-
-    keys = list(stack.keys())
-    values = list(stack.values())
-
-    for i in range(len(stack) - quotes_length, len(stack), step):
-        open = values[i]
-        close = values[i + step - 1]
-        high = max(values[i:i + step - 1])
-        low = min(values[i:i + step - 1])
-        if os.name == 'nt':  # windows
-            quotes.append(Quote(
-                date=datetime.fromtimestamp(keys[i]),
-                open=str(open).replace('.', ','),
-                high=str(high).replace('.', ','),
-                low=str(low).replace('.', ','),
-                close=str(close).replace('.', ','),
-                volume=None))
-        else:
-             quotes.append(Quote(
-                date=datetime.fromtimestamp(keys[i]),
-                open=open,
-                high=high,
-                low=low,
-                close=close,
-                volume=None))
-    return quotes
 
 
 def get_data(stack):
@@ -183,9 +113,9 @@ def get_data(stack):
         try:
             row = []
             for j in range(0, PREVIOUS + STEP, STEP):
-                row.append(values[i - j])
+                row.append(values[i - j])  # get previous 21 prices
             row.reverse()
-            row.append(1 if values[i] >= values[i + STEP] else 0)
+            row.append(1 if values[i] >= values[i + STEP] else 0)  # append result: 1 if put success, 0 otherwise
             data.append(row)
         except:
             pass
@@ -201,23 +131,23 @@ def get_last_row(stack):
     return row
 
 
-def check_indicators(stack):
-    data = get_data(stack)
+def check_prediction(stack):
+    data = get_data(stack)  # get previous 20 prices and the current one with profit column
     df = pd.DataFrame(data, columns=HEADER)
 
-    X = df.iloc[:, :21]
-    y = df['profit']
+    X = df.iloc[:, :21]  # get all prices without profit column
+    y = df['profit']  # get only profit column
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     model = LogisticRegression(max_iter=10000)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    model_accuracy = accuracy_score(y_test, y_pred)
+    model_accuracy = accuracy_score(y_test, y_pred)  # get accuracy
     print(f'Accuracy: {round(model_accuracy, 2)}, len_stack: {len(stack)}')
 
-    df2 = pd.DataFrame([get_last_row(stack), ], columns=HEADER[:-1])
-    y_pred = model.predict(df2)
+    df2 = pd.DataFrame([get_last_row(stack), ], columns=HEADER[:-1])  # create df only with the last row
+    y_pred = model.predict(df2)  # predict its profit
 
     if y_pred[-1] == 0:
         print(f'Do call by prediction')
@@ -225,93 +155,6 @@ def check_indicators(stack):
     else:
         print(f'Do put by prediction')
         do_action('put')
-
-    # quotes = get_quotes(stack, PERIOD)
-    # last_value = list(stack.values())[-1]
-    # sma_long = indicators.get_sma(quotes, lookback_periods=SMA_LONG)
-    # sma_short = indicators.get_sma(quotes, lookback_periods=SMA_SHORT)
-    # smma_short = indicators.get_smma(quotes, lookback_periods=SMMA_SHORT)
-    # cci = indicators.get_cci(quotes, CCI_PERIOD)
-    # psar = indicators.get_parabolic_sar(quotes)
-    # macd = indicators.get_macd(quotes)
-    # adx = indicators.get_adx(quotes)
-    # rsi = indicators.get_rsi(quotes)
-    # bbands = indicators.get_bollinger_bands(quotes)
-
-    # PREDICT
-
-    # scaler = StandardScaler()
-    # df = pd.DataFrame([get_data(),])
-    # scaled_data = df  # scaler.fit_transform(df)
-    # df = pd.DataFrame([get_data(),], columns=header[:-1])
-    # y_pred = MODEL.predict(df)
-
-    # ### MASS
-    # try:
-    #     start = driver.find_element(by=By.CLASS_NAME, value='start').text
-    #     start = int(start.replace('%', ''))
-    # except:
-    #     return
-    # if start > 97:
-    #     print(f'Do call by MASS and {100 - start}% agree with me')
-    #     do_action('call')
-    # elif start < 3:
-    #     print(f'Do put by MASS and {start}% agree with me')
-    #     do_action('put')
-    # return
-    # ### MASS END
-
-    ### GATHER STATISTICS
-    # action = 'put'  # random.choice(['call', 'put'])
-    # do_action(action)
-    # mass = start if action == 'put' else 100 - start
-    # gather_statistics(last_value, action, adx[-1], cci[-1], rsi[-1], macd[-1], psar[-1], mass)
-
-    # try:
-    #     print(f'ADX: {adx[-1].adx}')
-    #     if last_value < bbands[-1].lower_band:
-    #         if adx[-1].mdi < 27:
-    #             print(f'Do call BBands')
-    #             do_action('call')
-    #         else:
-    #             print(f'Do put BBands > 27')
-    #             do_action('put')
-    #     elif last_value > bbands[-1].upper_band:
-    #         if adx[-1].pdi < 27:
-    #             print(f'Do put BBands')
-    #             do_action('put')
-    #         else:
-    #             print(f'Do call BBands < 27')
-    #             do_action('call')
-    # except Exception as e:
-    #     print(e)
-
-    # try:
-    #     # if macd[-1].macd > 1 or macd[-1].macd < -1:
-    #     #     print('Inadequate MACD, pass')
-    #     #     return
-    #     if psar[-1].is_reversal:
-    #         # if adx[-1].pdi > 30 and adx[-1].pdi > adx[-1].mdi:
-    #         #     print(f'Do call by PSAR reversal with adx.pdi: {adx[-1].pdi}')
-    #         #     do_action('call')
-    #         # elif adx[-1].mdi > 30 and adx[-1].mdi > adx[-1].pdi:
-    #         #     print(f'Do put by PSAR reversal with adx.mdi: {adx[-1].mdi}')
-    #         #     do_action('put')
-    #         # if adx[-1].pdi > adx[-2].pdi and adx[-1].mdi < adx[-2].mdi:
-    #         #     print(f'Do call by PSAR reversal with PDI > MDI')
-    #         #     do_action('call')
-    #         # elif adx[-1].mdi > adx[-2].mdi and adx[-1].pdi < adx[-2].pdi:
-    #         #     print(f'Do put by PSAR reversal with MDI > PDI')
-    #         #     do_action('put')
-    #         if psar[-1].sar < last_value:
-    #             print(f'Do call by PSAR reversal with adx: {adx[-1].adx}')
-    #             do_action('call')
-    #         elif psar[-1].sar > last_value:
-    #             print(f'Do put by PSAR reversal with adx: {adx[-1].adx}')
-    #             do_action('put')
-    #
-    # except Exception as e:
-    #     print(e)
 
 
 def check_closed_trades():
@@ -329,13 +172,12 @@ def check_closed_trades():
                     if CURRENCY in closed_trades_currencies[i].text:
                         minus_trades += 1
             if minus_trades == CLOSED_TRADES_LENGTH:
-                # winsound.Beep(880, 100)
                 change_currency()
     except Exception as e:
         pass
 
 
-def WebSocketLog(stack):
+def websocket_log(stack):
     try:
         estimated_profit = driver.find_element(by=By.CLASS_NAME, value='estimated-profit-block__text').text
         if estimated_profit != '+92%':
@@ -402,10 +244,10 @@ def WebSocketLog(stack):
                 stack = {}  # refresh then
             if len(stack) >= LENGTH_STACK_MIN:
                 # check_closed_trades()  # TODO: enable it
-                check_indicators(stack)
+                check_prediction(stack)
     return stack
 
 
-LoadWebDriver()
+load_web_driver()
 while True:
-    STACK = WebSocketLog(STACK)
+    STACK = websocket_log(STACK)
