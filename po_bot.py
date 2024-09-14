52ffc579f4f59901ec091002a38f49a2c03ecd1a
Vitaly Svyatyuk <vitalysvyatyuk@gmail.com>
2024-09-14 16:25:26 +0300

    Fix Martingale

--- po_bot.py
+++ po_bot.py
@@ -158,13 +158,13 @@ def check_values(stack):
                 amount = driver.find_element(by=By.CSS_SELECTOR, value='#put-call-buttons-chart-1 > div > div.blocks-wrap > div.block.block--bet-amount > div.block__control.control > div.control__value.value.value--several-items > div > input[type=text]')
                 amount_value = int(amount.get_attribute('value')[1:])
                 base = '#modal-root > div > div > div > div > div.trading-panel-modal__in > div.virtual-keyboard.js-virtual-keyboard > div > div:nth-child(%s) > div'
-                if '0.00' not in last_split[4]:  # win
+                if '$0' != last_split[4]:  # win
                     if amount_value > 1:
                         amount.click()
                         hand_delay()
                         driver.find_element(by=By.CSS_SELECTOR, value=base % NUMBERS['1']).click()
                         AMOUNTS = get_amounts(float(deposit.text))  # refresh amounts
-                elif '0.00' not in last_split[3]:  # draw
+                elif '$0' != last_split[3]:  # draw
                     pass
                 else:  # lose
                     amount.click()
